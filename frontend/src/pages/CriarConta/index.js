a980c0324a131a0b629f85e7b41c7da085b1a4ca
Diego Oliveira <diegolvr14@gmail.com>
2022-10-17 01:19:04 -0300

    ?

---
+++ frontend/src/pages/CriarConta/index.js
@@ -0,0 +1,69 @@
+import React,{useState,useEffect} from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import api from '../../services/api'
+import './style.css';
+
+export default function Profile(){
+    const {id} = useParams();
+    const navigate = useNavigate();
+    const initUser={
+        name:'',
+        email: '',
+        empresa:''
+    }
+    const[user,setUser] = useState(initUser);
+
+    useEffect(()=>{
+       if(id){
+        api.get(`/users/${id}`).then(response=>{
+            setUser(...response.data)
+        })
+       } 
+    });
+
+    function onSubmit(ev){
+        ev.preventDefault();
+        const method = id ? 'put' : 'post';
+        const url = id 
+        ? `/users/${id}` 
+        : '/users';
+        
+        api[method](url,user).then((response)=>{
+            navigate('/')
+        })
+    }
+
+    function onChange(ev){
+        const {name,value} = ev.target;
+        setUser({...user,[name]:value})
+    }
+
+    return(
+        <div className="profile-container-main">
+            <div className="profile-container">
+                <form onSubmit={onSubmit} className="form">
+
+                    <h1>Criar Conta</h1>
+
+                    <h3>Nome:</h3>
+                    <input name="name" onChange={onChange} value={user.name}/>
+
+                    <h3>Email:</h3>
+                    <input type="email" name="email" onChange={onChange} value={user.email}/>
+
+                    <h3>Senha:</h3>
+                    <input type="password" name="password" onChange={onChange} value={user.empresa}/>
+
+                    <div className="actions">
+                        <Link className="buttonvoltar" to={('/')}>Voltar</Link>
+                        
+                    </div>
+                    
+                    <button className="buttoncriar" type="submit">Criar</button>
+
+                </form>
+            </div>    
+                
+        </div>
+    );
+}
---
+++ frontend/src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import CriarConta from './pages/CriarConta';
+import CriarVaga from './pages/CriarVagas';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+
+export default function Routess(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Login/>}/>
+                <Route exact path="/createuser" element={<CriarConta/>}/>
+                <Route exact path="/vagas" element={<CriarVaga/>}/>
+                <Route path="/create" element={<Profile/>}/>
+                <Route path="/update/:id" element={<Profile/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
